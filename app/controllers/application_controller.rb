6bd5e5e5cc6a9fecf27c7cea6d5dd4667d178c21
yuimusic551-hub <yuimusic551@gmail.com>
2026-05-23 17:33:50 +0900

    編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  
+  before_action :basic_auth, if: :production_environment?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
@@ -9,9 +11,20 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # 名前の登録を許可する魔法の言葉
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+  def production_environment?
+    Rails.env.production?
   end
 end
